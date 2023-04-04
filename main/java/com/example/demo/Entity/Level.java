20df508a0a08ac7e1a34994e30ee124390bcb04e
umesh <umeshsubhashm@gmail.com>
2023-04-04 22:33:43 +0530

    firest commit

---
+++ main/java/com/example/demo/Entity/Level.java
@@ -0,0 +1,18 @@
+/*
+ * package com.example.demo.Entity;
+ * 
+ * import javax.persistence.Entity; import javax.persistence.GeneratedValue;
+ * import javax.persistence.GenerationType; import javax.persistence.Id; import
+ * javax.persistence.Table;
+ * 
+ * @Entity
+ * 
+ * @Table(name = "levelv") public class Level {
+ * 
+ * @Id
+ * 
+ * @GeneratedValue(strategy = GenerationType.AUTO) private int levelid; private
+ * String
+ * 
+ * }
+ */
